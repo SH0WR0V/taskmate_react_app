3fca5747572cac86bb0541570eb7aec5cdb5b0bd
SH0WR0V <sm.shahriar1230@gmail.com>
2024-04-29 19:49:50 +0600

    theme change functionality added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react";
 import Logo from "../assets/logo.png";
 
 export const Header = () => {
+  const [theme, setTheme] = useState(
+    JSON.parse(localStorage.getItem("theme")) || "light"
+  );
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme));
+    document.documentElement.removeAttribute("class");
+    document.documentElement.classList.add(theme);
+  }, [theme]);
   return (
     <header>
       <div className="logo">
@@ -8,12 +17,18 @@ export const Header = () => {
         <span>Taskmate</span>
       </div>
       <div className="themeSelector">
-        <span className="light"></span>
-        <span className="medium"></span>
-        <span className="dark activeTheme"></span>
-        <span className="gOne"></span>
-        <span className="gTwo"></span>
-        <span className="gThree"></span>
+        <span
+          onClick={() => setTheme("light")}
+          className={theme === "light" ? "light activeTheme" : "light"}
+        ></span>
+        <span
+          onClick={() => setTheme("medium")}
+          className={theme === "medium" ? "medium activeTheme" : "medium"}
+        ></span>
+        <span
+          onClick={() => setTheme("dark")}
+          className={theme === "dark" ? "dark activeTheme" : "dark"}
+        ></span>
       </div>
     </header>
   );
